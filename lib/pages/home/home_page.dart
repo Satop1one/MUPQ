b6fbc03be7c683ae2de3ca6d8dcbf3c8e8690abc
satop1one <121037921@upq.edu.mx>
2022-11-26 13:25:55 -0600

    Se modifico en la parte 10

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -7,11 +7,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Mupq')
-      ),
-      body: Center(
-        child: Text('Home Page')
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  'assets/img/upq.png',
+                  width: 150,
+                  height: 150,
+                  ),
+                Text('Muevete UPQ'),
+              ],
+            ),
+            SizedBox(height: 50),
+            Text('Selecciona tu Rol'),
+            SizedBox(height: 30,),
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/img/pasajero.png',),
+              radius: 100,
+              backgroundColor: Colors.white,
+            ),
+            SizedBox(height: 10,),
+            Text('Pasajero'),
+            SizedBox(height: 30,),
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/img/conductor.png',),
+              radius: 100,
+              backgroundColor: Colors.white,
+            ),
+            SizedBox(height: 10,),
+            Text('Conductor')
+          ],
+        ),
       ),
     );
   }
