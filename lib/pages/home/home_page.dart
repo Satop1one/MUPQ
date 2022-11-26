136c6269459220c23a3029e6ed5b16701f8ee52b
satop1one <121037921@upq.edu.mx>
2022-11-26 14:40:05 -0600

    Se actualizo al paso 11

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage
@@ -8,38 +9,74 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset(
-                  'assets/img/upq.png',
-                  width: 150,
-                  height: 150,
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Color(0xff8C2939) , Color(0xff18295B)]
+            ),
+          ),
+          child: Column(
+            children: [
+              ClipPath(
+                clipper: DiagonalPathClipperTwo(),
+                child: Container(
+                  color: Colors.white,
+                  height: MediaQuery.maybeOf(context)!.size.height*0.16,
+                  child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset(
+                      'assets/img/upq.png',
+                      width: 120,
+                      height: 140,
+                      ),
+                    const Text('Muevete UPQ'),
+                  ],
+              ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              const Text(
+                  'Bienvenid@ Cardenal',
+                    style: TextStyle(
+                    color: Colors.white
                   ),
-                Text('Muevete UPQ'),
-              ],
-            ),
-            SizedBox(height: 50),
-            Text('Selecciona tu Rol'),
-            SizedBox(height: 30,),
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/img/pasajero.png',),
-              radius: 100,
-              backgroundColor: Colors.white,
-            ),
-            SizedBox(height: 10,),
-            Text('Pasajero'),
-            SizedBox(height: 30,),
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/img/conductor.png',),
-              radius: 100,
-              backgroundColor: Colors.white,
-            ),
-            SizedBox(height: 10,),
-            Text('Conductor')
-          ],
+                ),
+                const Text(
+                  'Selecciona una opcion',
+                    style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+              const SizedBox(height: 30,),
+              const CircleAvatar(
+                backgroundImage: AssetImage('assets/img/pasajero.png',),
+                radius: 100,
+                backgroundColor: Colors.black,
+              ),
+              const SizedBox(height: 10,),
+              const Text(
+              'Pasajero',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+              ),
+              const SizedBox(height: 30,),
+              const CircleAvatar(
+                backgroundImage: AssetImage('assets/img/conductor.png',),
+                radius: 100,
+                backgroundColor: Colors.black,
+              ),
+              const SizedBox(height: 10,),
+              const Text('Conductor',
+                style: TextStyle(
+                  color: Colors.white
+                  ),
+              )
+            ],
+          ),
         ),
       ),
     );
